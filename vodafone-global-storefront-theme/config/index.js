9b08c9508f580dc9f2806795348c4e97c1799f8e
Ökrös <okrosc@E0619228.local>
2020-08-31 11:38:19 +0200

    Customize marketplace for Italian requirements

---
+++ vodafone-global-storefront-theme/config/index.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const config = {
+	env: "dev",
+	theme: "vodafone",
+	marketplacePath: "https://testvfitaly.appdirect.com",
+	// marketplacePath: "https://cloud-apps.vodafone.com",
+	// pageObjectsPath: "https://marketplace.appdirect.com/api/internal/storefront/v1",
+	pageObjectsPath: "https://testvfitaly.appdirect.com/api/internal/storefront/v1",
+	publicCSSGenPath: "/assets/css/pages",
+	publicJSPath: "/assets/css/images",
+	publicImagesPath: "/assets/js",
+	supportedFontExtensions: ["woff"]
+}
+
+module.exports = config
